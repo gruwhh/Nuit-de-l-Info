7e665bf77c6033cabbee20170211202e1b2f0a95
Cryoz-info <amiraultpatrick17@gmail.com>
2025-12-04 23:09:45 +0100

    Porte clickable

--- Page web/script/Script.js
+++ Page web/script/Script.js
@@ -6,3 +6,7 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
 }
+
+function ouvrirIndex2() {
+    window.location.href = "index2.html"; // redirection au clic
+}
